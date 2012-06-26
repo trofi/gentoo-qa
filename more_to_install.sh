5fa256434e16a328aea72becc4d0ee23761b1d91
Sergei Trofimovich <slyfox@gentoo.org>
2012-06-26 09:44:40 +0300

    more_to_install.sh: script to get not-installed-yet packages
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

---
+++ more_to_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# script to get list of not-installed-yet packages
+# from specified overlay
+
+overlay_name=${1-gentoo-haskell}
+{
+  # get all packages
+  eix --only-names             --in-overlay ${overlay_name}
+  # get installed packages
+  eix --only-names --installed --in-overlay ${overlay_name}
+
+# print only unique (aka not installed)
+} | sort | uniq -u
