1d65689e82a09b6a5441d63a8ff8b4c3f20dfffd
Sergei Trofimovich <slyfox@gentoo.org>
2014-08-10 15:15:02 +0300

    check_description.sh: don't fix "etc." case (thanks to Arfrever)
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- check_description.sh
+++ check_description.sh
@@ -4,6 +4,10 @@ find . -type f -name '*.ebuild' | while read e
 do
     while read l
     do
+        # 20 ebuilds have "etc."
+        # http://git.overlays.gentoo.org/gitweb/?p=proj/portage.git;a=commitdiff;h=bbb34efebd0bfc0b231073d00b863b3e3ebd918a
+        [[ $l == 'DESCRIPTION='*'etc."' ]] && continue
+
         if [[ $l == 'DESCRIPTION='*'."' ]]; then
             echo "$e: fixing '.' in description"
             sed -i -e '/^DESCRIPTION=/ { s/."$/"/ }' "$e"
