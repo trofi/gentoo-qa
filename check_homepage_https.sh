d3820951867328d62b1ce3d1746bc92641bc53a0
Sergei Trofimovich <siarheit@google.com>
2015-09-21 09:27:11 +0100

    check_homepage_https.sh: adds https URL to github homepage
    Signed-off-by: Sergei Trofimovich <siarheit@google.com>

---
+++ check_homepage_https.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+check_homepage() {
+    local hp=$1
+
+    hsp=${hp/\"http:/\"https:}
+
+    if [[ $pn != $hp ]]; then
+        echo "FIX HOMEPAGE HTTPS: $e"
+        sed -e "/HOMEPAGE/ s/\"http:/\"https:/" -i "${e}"
+    fi
+}
+
+find . -type f -name '*.ebuild' | while read e
+do
+    homepage=
+
+    while read l
+    do
+        [[ $l == *"HOMEPAGE="*"http://github.com/"* ]] && homepage=$l
+
+        if [[ -n $homepage ]]; then
+            check_homepage "$homepage"
+            break
+        fi
+    done < "$e"
+done
