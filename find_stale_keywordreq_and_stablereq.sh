a8ab17063e797fd1131fad0be2e63ba7d27ba26d
Sergei Trofimovich <slyfox@gentoo.org>
2020-05-17 10:41:17 +0100

    find_stale_keywordreq_and_stablereq.sh: allow ALLARCHES KEYWORDREQ
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- find_stale_keywordreq_and_stablereq.sh
+++ find_stale_keywordreq_and_stablereq.sh
@@ -178,7 +178,7 @@ find_stale_bugs_for_keyword() {
                 bug=${line#\# bug #}
                 stale_bug=yes
                 ;;
-            '# bug '*' (KEYWORDREQ)'|'# bug '*' (STABLEREQ)'|'# bug '*' (STABLEREQ) ALLARCHES')
+            '# bug '*' (KEYWORDREQ)'|'# bug '*' (KEYWORDREQ) ALLARCHES'|'# bug '*' (STABLEREQ)'|'# bug '*' (STABLEREQ) ALLARCHES')
                 bug=${line#\# bug }
                 bug=${bug// *}
                 stale_bug=yes
