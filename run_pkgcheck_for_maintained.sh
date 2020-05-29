54298701ff0e631bca5dee29fe0561a55bfc68eb
Sergei Trofimovich <slyfox@gentoo.org>
2020-05-29 23:00:24 +0100

    run_pkgcheck_for_maintained.sh: ignore UnstableOnly / PotentialStable false positives
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- run_pkgcheck_for_maintained.sh
+++ run_pkgcheck_for_maintained.sh
@@ -6,6 +6,23 @@
 : ${PROJECTS:=}
 : ${REPO:=gentoo}
 
+: ${DISABLE_EXTRA_KEYWORDS=}
+
+DISABLED_KEYWORDS=(
+    # Full of false positives:
+    #  https://github.com/pkgcore/pkgcheck/issues/230
+    UnstableOnly
+    PotentialStable
+)
+
+DISABLED_KEYWORDS+=(
+    ${DISABLE_EXTRA_KEYWORDS}
+)
+
+# -foo,-bar
+keywords=${DISABLED_KEYWORDS[@]/#/-}
+keywords=${keywords// /,}
+
 # Grab project list from:
 #     https://wiki.gentoo.org/wiki/User:Slyfox
 emails=(
@@ -34,4 +51,4 @@ emails=(
 
 IFS=,
 # (foo bar) -> "foo@bentoo.org,bar@gentoo.org"
-portageq --no-regex --no-version --maintainer-email="${emails[*]/%/@gentoo.org}" --repo=${REPO} | xargs --no-run-if-empty pkgcheck scan "$@" --repo=gentoo
+portageq --no-regex --no-version --maintainer-email="${emails[*]/%/@gentoo.org}" --repo=${REPO} | xargs --no-run-if-empty pkgcheck scan --keywords="${keywords}" "$@" --repo=gentoo
