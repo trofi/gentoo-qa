07acea063ded2587b2640c28e98fecbe3211d926
Sergei Trofimovich <slyfox@gentoo.org>
2020-09-04 09:28:27 +0100

    find_stale_keywordreq_and_stablereq.sh: disable parallelism
    Now bugzilla does not allow for parallel access and
    natka does not know how to retry gracefully.
    
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- find_stale_keywordreq_and_stablereq.sh
+++ find_stale_keywordreq_and_stablereq.sh
@@ -102,8 +102,8 @@ refresh_lists() {
 
     local arch
     for arch in "${ARCHES[@]}"; do
-        nattka ${EXTRA_NATTKA_PARAMS} apply --stablereq  --arch "${arch}" ${EXTRA_NATTKA_APPLY_PARAMS} -n > "$(stable_file   "${arch}")" &
-        nattka ${EXTRA_NATTKA_PARAMS} apply --keywordreq --arch "${arch}" ${EXTRA_NATTKA_APPLY_PARAMS} -n > "$(keywords_file "${arch}")" &
+        nattka ${EXTRA_NATTKA_PARAMS} apply --stablereq  --arch "${arch}" ${EXTRA_NATTKA_APPLY_PARAMS} -n > "$(stable_file   "${arch}")"
+        nattka ${EXTRA_NATTKA_PARAMS} apply --keywordreq --arch "${arch}" ${EXTRA_NATTKA_APPLY_PARAMS} -n > "$(keywords_file "${arch}")"
     done
     wait
 }
