965e6e735d2401917e9a2dc706b9043a46f88cca
Sergei Trofimovich <slyfox@gentoo.org>
2020-12-20 10:35:28 +0000

    more_to_install.sh: ignore masked packages
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- more_to_install.sh
+++ more_to_install.sh
@@ -6,9 +6,15 @@
 overlay_name=${1-haskell}
 {
   # get all packages
-  eix --only-names             --in-overlay ${overlay_name}
+  eix --only-names --non-masked             --in-overlay ${overlay_name}
   # get installed packages
-  eix --only-names --installed --in-overlay ${overlay_name}
+  eix --only-names --non-masked --installed --in-overlay ${overlay_name}
 
+  # --non-masked should have done it but does not for some reason.
+  # get masked packages, avoid installing them
+  EROOT=$(portageq envvar EROOT)
+  REPO_ROOT=$(portageq get_repo_path "${EROOT}" ${overlay_name})
+  sed -e 's/\s*#.*//g' "$REPO_ROOT"/profiles/package.mask | grep -v '^$'
+  sed -e 's/\s*#.*//g' "$REPO_ROOT"/profiles/package.mask | grep -v '^$'
 # print only unique (aka not installed)
 } | sort | uniq -u
